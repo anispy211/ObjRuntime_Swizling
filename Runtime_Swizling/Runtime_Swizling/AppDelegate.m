86c664ffa4ef5d54a6925cb88e34d160d7a6bd78
codewalla <inmac017@in-mac-018.local>
2014-03-25 18:23:57 +0530

    Initial commit2

---
+++ Runtime_Swizling/Runtime_Swizling/AppDelegate.m
@@ -0,0 +1,60 @@
+//
+//  AppDelegate.m
+//  Runtime_Swizling
+//
+//  Created by codewalla on 25/03/14.
+//  Copyright (c) 2014 Aniruddha. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <objc/message.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Implementation exchange of two method
+    
+    [self swizle];       //objc_msgSend(self, @selector(swizle));   Message passing in Objective c runtime
+
+    
+    //[self performSubtraction];  
+    objc_msgSend(self, @selector(performSubtraction)); //  Message passing in Objective c runtime
+    
+   // the above function calls performAddition selector .It is same as calling [self performSubstraction];
+    
+    return YES;
+}
+
+
+- (void)performAddition
+{
+    NSLog(@"+++++++++++++++++++ Addition ++++++++++++++++++");
+
+}
+
+- (void)performSubtraction
+{
+    NSLog(@"-------------------- Substract -------------------------");
+}
+
+
+
+
+
+- (void)swizle
+{
+    SEL first = @selector(performAddition);  // First method
+    SEL Second = @selector(performSubtraction); // Second method
+
+   // Get Instance method
+    Method m1 = class_getInstanceMethod([self class], first);
+    Method m2 = class_getInstanceMethod([self class], Second);
+    
+    // Exchange implementation
+    method_exchangeImplementations(m1, m2);
+    
+}
+
+
+@end
---
+++ Runtime_Swizling/Runtime_Swizling/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Runtime_Swizling
+//
+//  Created by codewalla on 25/03/14.
+//  Copyright (c) 2014 Aniruddha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+
+
+
